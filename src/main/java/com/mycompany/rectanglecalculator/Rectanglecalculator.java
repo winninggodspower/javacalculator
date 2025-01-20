100efac331f399420a1296f7d9715f6f18489b35
winnning godspower <winnninggodspower@gmail.com>
2025-01-20 17:59:54 +0100

    initial commit

---
+++ src/main/java/com/mycompany/rectanglecalculator/Rectanglecalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rectanglecalculator;
+
+/**
+ *
+ * @author USER PC
+ */
+public class Rectanglecalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
